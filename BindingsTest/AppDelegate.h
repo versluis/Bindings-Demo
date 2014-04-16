a52b84fe13c3c7134664d8ae6a6997588b45b46b
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2014-04-16 19:29:52 -0400

    Initial Commit

---
+++ BindingsTest/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  BindingsTest
+//
+//  Created by Jay Versluis on 16/04/2014.
+//  Copyright (c) 2014 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ BindingsTestTests/BindingsTestTests.m
@@ -0,0 +1,34 @@
+//
+//  BindingsTestTests.m
+//  BindingsTestTests
+//
+//  Created by Jay Versluis on 16/04/2014.
+//  Copyright (c) 2014 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BindingsTestTests : XCTestCase
+
+@end
+
+@implementation BindingsTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
