03f532032bb29ac5170f2e43f46cba180f2b67e2
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2014-04-16 19:45:47 -0400

    as demonstrated in the screencast

---
+++ BindingsTest/Event.h
@@ -0,0 +1,17 @@
+//
+//  Event.h
+//  BindingsTest
+//
+//  Created by Jay Versluis on 16/04/2014.
+//  Copyright (c) 2014 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Event : NSManagedObject
+
+@property (nonatomic, retain) NSString * title;
+
+@end
---
+++ BindingsTest/Event.m
@@ -0,0 +1,16 @@
+//
+//  Event.m
+//  BindingsTest
+//
+//  Created by Jay Versluis on 16/04/2014.
+//  Copyright (c) 2014 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import "Event.h"
+
+
+@implementation Event
+
+@dynamic title;
+
+@end
